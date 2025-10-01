6c4ba6a4a77f5ffd44af3f674fc170d83912663a
kanishka610 <148204797+kanishka610@users.noreply.github.com>
2025-10-01 20:18:07 +0530

    Add files via upload

---
+++ Exercise-1/Creational Design Pattern/Factory/FactoryNotificationDemo.java
@@ -0,0 +1,12 @@
+// Client
+public class FactoryNotificationDemo {
+    public static void main(String[] args) {
+        NotificationFactory factory = new NotificationFactory();
+
+        Notification n1 = factory.createNotification("EMAIL");
+        n1.notifyUser();
+
+        Notification n2 = factory.createNotification("SMS");
+        n2.notifyUser();
+    }
+}
