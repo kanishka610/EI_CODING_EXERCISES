aec31e8319f670d59335f0e86cf7a93b29579c63
kanishka610 <148204797+kanishka610@users.noreply.github.com>
2025-10-01 20:05:24 +0530

    Add files via upload

---
+++ Exercise-1/Structural Design Pattern/Bridge/BridgeDemo.java
@@ -0,0 +1,9 @@
+public class BridgeDemo {
+    public static void main(String[] args) {
+        Shape redCircle = new Circle(new Red());
+        Shape blueSquare = new Square(new Blue());
+
+        redCircle.draw();
+        blueSquare.draw();
+    }
+}
