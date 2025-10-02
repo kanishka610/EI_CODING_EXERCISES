0d3bc66a21b877aa8b81b80169675cceb5a91633
kanishka610 <148204797+kanishka610@users.noreply.github.com>
2025-10-02 09:38:55 +0530

    Update Stage.java

--- Exercise-2/Rocket_Launch_Simulator/Rocket/Stage.java
+++ Exercise-2/Rocket_Launch_Simulator/Rocket/Stage.java
@@ -1,1 +1,20 @@
+package com.rocketlauncher.rocket;
+
+public class Stage {
+    private int number;
+    private int fuel;
+
+    public Stage(int number, int fuel) {
+        this.number = number;
+        this.fuel = fuel;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+}
 
